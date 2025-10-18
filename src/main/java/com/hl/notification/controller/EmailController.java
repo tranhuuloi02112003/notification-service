ff567508728553fb529299b424afa2e5d72304ad
tranhuuloi02112003 <tranhuuloi2k3@gmail.com>
2025-10-18 13:25:13 +0700

    feat: implement api send email

---
+++ src/main/java/com/hl/notification/controller/EmailController.java
@@ -0,0 +1,21 @@
+package com.hl.notification.controller;
+
+import com.hl.notification.dto.SendEmailRequest;
+import com.hl.notification.service.mail.EmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/emails")
+@RequiredArgsConstructor
+public class EmailController {
+    private final EmailService emailService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void send(@RequestBody SendEmailRequest sendEmailRequest) {
+        emailService.sendMail(sendEmailRequest);
+    }
+}
+
